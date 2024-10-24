effd03860a39b73a22e8e742f01fd0e8892ef741
Gilles Delpy SOP <165911061+Ward0007@users.noreply.github.com>
2024-10-24 14:19:01 +0200

    Update PutOption.cpp

--- Partie 2/PutOption.cpp
+++ Partie 2/PutOption.cpp
@@ -1,11 +1,18 @@
 #include "PutOption.h"
 
-PutOption::PutOption()
-{
+PutOption::PutOption() {}
 
+PutOption::PutOption(double _Expiry, double _Strike) : EuropeanVanillaOption(_Expiry, _Strike) {}
+
+double PutOption::payoff(double z) {
+    if (_strike >= z) {
+        return _strike - z;
+    } else {
+        return 0;
+    }
 }
 
-PutOption::~PutOption()
-{
-
+PutOption::optionType PutOption::GetOptionType() {
+    return optionType::put;
 }
+PutOption::~PutOption() {}
