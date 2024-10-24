416727ac6e92c91341d0610d89b034d87670981a
Gilles Delpy SOP <165911061+Ward0007@users.noreply.github.com>
2024-10-24 14:18:28 +0200

    Update PutOption.h

--- Partie 2/PutOption.h
+++ Partie 2/PutOption.h
@@ -1,20 +1,17 @@
 #ifndef PUTOPTION_H
 #define PUTOPTION_H
+
 #include "EuropeanVanillaOption.h"
 #pragma once
 
 class PutOption : public EuropeanVanillaOption {
 public:
-    PutOption(double _Expiry, double _Strike) : EuropeanVanillaOption(_Expiry, _Strike) {}
-    double payoff(double z) {
-        if (_strike >= z){
-        return _strike-z;
-        }
-        else{return 0;}
-    }
-
-    override optionType GetOptionType(){
-        return put;
-    }
+    PutOption();
+    PutOption(double, double);
+    double payoff(double) override;
+    optionType GetOptionType() override;
+    ~PutOption();
 };
-#endif
+
+#endif 
+#include "PutOption.cpp"
