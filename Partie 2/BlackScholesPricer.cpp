e6cd0df31d268a8e87c0ed78bcc84890f20bad83
Barthelemy Bengloan <bartbengloan@icloud.com>
2024-10-31 21:42:44 +0100

    Update BlackScholesPricer.cpp

--- Partie 2/BlackScholesPricer.cpp
+++ Partie 2/BlackScholesPricer.cpp
@@ -18,7 +18,7 @@ double BlackScholesPricer::operator()() {
     double d1, d2, prix;
     d1= (1/(sigma * sqrt(T)))*(log(S / K) + (r + 0.5 * sigma*sigma) * T) ;
     d2 = d1 - sigma *sqrt(T);
-<<<<<<< HEAD
+
     if (Option_->GetOptionNature() == optionNature::vanille) {
         if (Option_->GetOptionType() == optionType::call) {
             prix = S * (1 - 0.5 * erfc(d1 / sqrt(2))) - K * exp(-r * T) * (1 - 0.5 * erfc(d2 / sqrt(2)));
@@ -36,13 +36,6 @@ double BlackScholesPricer::operator()() {
             prix= 0.5 * exp(-r * T) * std::erfc(d2 / sqrt(2));
         }
         return prix;
-=======
-    if (Option_->GetOptionType() == EuropeanVanillaOption::optionType::call) {
-        prix = S*0.5 * erfc(d1/sqrt(2)) - K * exp(-r * T) * 0.5 *erfc(d2 /sqrt(2));
-    } 
-    else {
-        prix = -S*0.5 * erfc(-d1/sqrt(2)) + K * exp(-r * T) *0.5 *erfc(-d2 /sqrt(2));
->>>>>>> d90387f8a3b3b54ee694a813bfdcf0e5eaab127c
     }
 }
 double BlackScholesPricer::delta(){
@@ -51,7 +44,6 @@ double BlackScholesPricer::delta(){
     double r = Interest_rate_;
     double T = Option_->getExpiry();
     double sigma = Volatility_;
-<<<<<<< HEAD
     double d1,d2,Delta;
     d1 = (1 / (sigma * sqrt(T))) * (log(S / K) + (r + 0.5 * sigma * sigma) * T);
     d2 = d1 - sigma * sqrt(T);
@@ -63,15 +55,7 @@ double BlackScholesPricer::delta(){
             Delta= -0.5 * std::erfc(-d1 / sqrt(2));
         }
         return Delta;
-=======
-    double d,Delta;
-    d= (log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma *sqrt(T));
-    if (Option_->GetOptionType() == EuropeanVanillaOption::optionType::call) {
-        Delta = 0.5 * erfc(d/sqrt(2));
-    } 
-    else {
-        Delta = 0.5 * erfc(d/sqrt(2))-1;
->>>>>>> d90387f8a3b3b54ee694a813bfdcf0e5eaab127c
+
     }
     else if (Option_->GetOptionNature() == optionNature::digital) {
         if (Option_->GetOptionType() == optionType::call) {
